89d3df8a83555ab0d8fc07af906d660ca4b0ba2e
闫强 <yanqiang@spap.com>
2022-05-20 15:41:15 +0800

    fix bugs

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -1,10 +1,9 @@
 #! /usr/bin/env node
 
+const importLocal = require("import-local");
 
-const importLocal = require('import-local')
-
-if (importLocal(__dirname)){
-    require('npmlog').info('cli', '正在使用 import-cli 本地版本')
+if (importLocal(__dirname)) {
+  require("npmlog").info("yq-cli-dev", "正在使用 yq-cli 本地版本");
 } else {
-    require('../lib')(process.argv.slice('2'))
+  require("../lib")(process.argv.slice("2"));
 }
